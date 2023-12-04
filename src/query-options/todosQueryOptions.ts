0fa86c8c1dc0a14fef16bd1675de207f1b68ad4b
Dan <dancoman@live.com>
2023-12-04 12:51:01 +0100

    fixed import

--- src/query-options/todosQueryOptions.ts
+++ src/query-options/todosQueryOptions.ts
@@ -1,5 +1,6 @@
 import { UseQueryOptions } from "@tanstack/react-query";
 import { getTodos } from "../api/todoService";
+import { queryKeys } from "../constants/queryKeys";
 import { Todo } from "../types";
 
 export const todosQueryOptions: UseQueryOptions<Todo[], Error> = {
