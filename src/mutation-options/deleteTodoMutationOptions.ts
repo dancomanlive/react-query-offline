ce52100652f25df62a6dfb6aeca85b707df62d62
Dan <dancoman@live.com>
2023-12-04 11:44:30 +0100

    added types

--- src/mutation-options/deleteTodoMutationOptions.ts
+++ src/mutation-options/deleteTodoMutationOptions.ts
@@ -1,14 +1,9 @@
-import { UseMutationOptions, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import * as todoService from "../api/todoService";
 import { useNetworkState } from "../hooks/useNetworkState";
-import { Todo } from "../types";
+import { MutationStateContext, Todo } from "../types";
 
-export function deleteTodoMutationOptions(): UseMutationOptions<
-  void,
-  Error,
-  number,
-  { previousTodos?: Todo[] }
-> {
+export function deleteTodoMutationOptions() {
   const queryClient = useQueryClient();
   const isConnected = useNetworkState();
   return {
@@ -17,7 +12,7 @@ export function deleteTodoMutationOptions(): UseMutationOptions<
     // The function to call when this mutation is executed. It calls the deleteTodo function from the todoService.
     mutationFn: todoService.deleteTodo,
     // Called immediately before the mutation function. Used for optimistic updates.
-    onMutate: async (todoId) => {
+    onMutate: async (todoId: number) => {
       // Cancel any ongoing refetch queries for todos to prevent them from overwriting our optimistic update.
       await queryClient.cancelQueries({ queryKey: ["todos"] });
 
@@ -39,7 +34,7 @@ export function deleteTodoMutationOptions(): UseMutationOptions<
     },
 
     // Called if the mutation encounters an error. Used for rolling back optimistic updates.
-    onError: (err, todoId, context) => {
+    onError: (context: MutationStateContext) => {
       // If there's a previous state available, revert to it to undo the optimistic update.
       if (context?.previousTodos) {
         queryClient.setQueryData(["todos"], context.previousTodos);
